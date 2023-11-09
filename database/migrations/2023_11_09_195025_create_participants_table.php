fd6f4b6e6a81987afa97597d9a01931572103b1c
hassan-aamer <hassanaamer048@gmail.com>
2023-11-09 22:57:58 +0200

    Create All Datebase

---
+++ database/migrations/2023_11_09_195025_create_participants_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->foreignId('conversation_id')->constrained('conversations')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->enum('role',['admen','member'])->default('member');
+            $table->timestamp('joined_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participants');
+    }
+};
