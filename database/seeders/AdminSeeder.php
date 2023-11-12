200a5c2291543b4226fa88f8ac1d32b0c6b40839
hassan-aamer <hassanaamer048@gmail.com>
2023-11-12 20:24:24 +0200

    Update

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'=>'Hassan',
+            'email'=>'hassan@gmail.com',
+            'password'=>'hassan123456',
+        ]);
+    }
+}
